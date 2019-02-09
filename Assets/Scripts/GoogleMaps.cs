c7e896fd8f1560239274e174fe117cb16e450312
Firifire <firifire@live.com>
2019-02-10 01:53:54 +0800

    Initial Commit

---
+++ Assets/Scripts/GoogleMaps.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoogleMaps : MonoBehaviour {
+
+    public RawImage img;
+
+    string url;
+
+    public float lat;
+    public float lon;
+
+    LocationInfo li;
+
+    public int zoom = 14;
+    public int mapWidth = 640;
+    public int mapHeight = 640;
+
+    public enum mapType { roadmap, satellite, hybrid, terrain }
+    public mapType mapSelected;
+    public int scale;
+
+
+    IEnumerator Map()
+    {
+        url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon +
+            "&zoom=" + zoom + "&size=" + mapWidth + "x" + mapHeight + "&scale=" + scale
+            + "&maptype=" + mapSelected +
+            "&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyAQuNjhWyRCgF4TDM5-lFSJslgQs5eZ_yA";
+        WWW www = new WWW(url);
+        yield return www;
+        img.texture = www.texture;
+        img.SetNativeSize();
+
+    }
+
+    IEnumerator Start()
+    {
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+            goto display;
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            goto display;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            goto display;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            lat = Input.location.lastData.latitude;
+            lon = Input.location.lastData.longitude;
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+
+display:
+        img = gameObject.GetComponent<RawImage>();
+        StartCoroutine(Map());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
